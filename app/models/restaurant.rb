2df529e74005b17f448fe01e515d6cccad709087
jerv <jeremy.venegas@enigmacreativegroup.com>
2015-06-25 15:17:07 -0700

    image stuff

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+	mount_uploader :image, ImageUploader
+end
+
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::Compatibility::Paperclip
 end
